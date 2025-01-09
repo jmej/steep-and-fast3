2fba69c23b498f9a2b2ff9b5768485c0fc6a6388
jmej <jmejia@anestheticaudio.com>
2025-01-09 11:29:50 -0800

    changed ellipse to rect

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
+let xLoc = 50;
+let xSpeed = 1;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  noLoop();
-  background(255, 0, 255);
-  translate(width/2, height/2);
-  beginShape();
-  for(let i = 0; i < 10; i++) {
-    const x = random(-100, 100);
-    const y = random(-100, 100);
-    vertex(x, y);
+  xLoc = xLoc + xSpeed;
+  if(xLoc > width - 50|| xLoc < 50){
+    xSpeed = xSpeed * -1;
   }
-  endShape();
+  background(0, 0, 255);
+  fill(255,0, 255);
+  rect(xLoc, height/2, 100, 100);
+  //print(xLoc);
 }
